46733004a404a02111734c606ebccf4c575ada9a
Carolina <carolinalimacvalladares@gmail.com>
2022-01-08 17:39:15 -0300

    Add image functionality on mobile layout

--- src/components/ProductPage.tsx
+++ src/components/ProductPage.tsx
@@ -55,15 +55,55 @@ const ProductPage = ({
     setCurrentImage(image);
   };
 
+  const prevImage = () => {
+    let prevImage: image;
+    const currentImageIndex = product.images.indexOf(currentImage);
+    const lastImage = product.images[product.images.length - 1];
+    if (currentImageIndex === 0) {
+      prevImage = lastImage;
+    } else {
+      prevImage = product.images[currentImageIndex - 1];
+    }
+
+    setCurrentImage(prevImage);
+  };
+
+  const nextImage = () => {
+    let nextImage: image;
+    const currentImageIndex = product.images.indexOf(currentImage);
+    const firstImage = product.images[0];
+    if (currentImageIndex === product.images.length - 1) {
+      nextImage = firstImage;
+    } else {
+      nextImage = product.images[currentImageIndex + 1];
+    }
+
+    setCurrentImage(nextImage);
+  };
+
   return (
     <div className={style["product-page"]}>
       <div className={style["product-page__wrapper"]}>
-        <div className={"product-page__images"}>
+        <div className={style["product-page__images"]}>
+          <button
+            onClick={prevImage}
+            className={`${style["images-button"]} ${style["prev-button"]}`}
+          >
+            <img src="./icons/icon-previous.svg" alt="previous" />
+          </button>
+
           <img
             className={style["product-image"]}
             src={currentImage.main}
             alt=""
           />
+
+          <button
+            onClick={nextImage}
+            className={`${style["images-button"]} ${style["next-button"]}`}
+          >
+            <img src="./icons/icon-next.svg" alt="next" />
+          </button>
 
           <ul className={style["image-preview-list"]}>
             {product.images.map((image) => (
